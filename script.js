b990312195c5df76758cbac48fd4bf3bc88bf87b
Tanatswa Mapfumo <99559726+tboysavage@users.noreply.github.com>
2024-05-23 01:27:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('a[href^="#"]');
+    for (const link of links) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    }
+});
